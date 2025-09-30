d9099dfbcc03dafd0cb58fbf9839b13132e22b86
nurulaisyah19 <nurul.aisah3000@gmail.com>
2025-09-30 23:50:15 +0700

    Selesai Langkah Praktikum Modul 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
+
     protected $table = 'user';
     protected $guarded = ['id'];
 
@@ -16,4 +17,11 @@ class UserModel extends Model
         return $this->belongsTo(Kelas::class, 'kelas_id');
     }
 
+    // Tambahan method untuk join tabel user dengan kelas
+    public function getUser()
+    {
+        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                    ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+                    ->get();
+    }
 }
--- app/Models/kelas.php
+++ app/Models/kelas.php
@@ -14,4 +14,9 @@ class kelas extends Model
     {
         return $this->hasMany(UserModel::class, 'kelas_id');
     }
+
+    public function getKelas()
+    {
+        return $this->all();
+    }
 }
